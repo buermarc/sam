2ec9aa6dcb0f65cb59519f32173c00955ebc1a49
mulc <buermarc2@gmail.com>
2020-05-13 23:33:59 +0200

    package.json emu and del unnecessary controller

--- src/emulate/abgabe.js
+++ src/emulate/abgabe.js
@@ -2,36 +2,48 @@
 const fetch = require("node-fetch");
 async function a () {
 
-    let request = {
-        name: "NewGame",
-        teams: ["/api/team/1", "/api/team/2"],
-        winnerTeam: "/api/team/2", 
-        division: "/api/division/1",
+    async function first() {
+        let request = {
+            name: "NewGame",
+            teams: ["/api/team/1", "/api/team/2"],
+            winnerTeam: "/api/team/2", 
+            division: "/api/division/1",
+        }
+
+        let response = await fetch("http://localhost:9999/api/liga/", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept-Content-Type": "application/json"
+            },
+        });
+
+        let json = await response.json();
+        console.log(JSON.stringify(json, null, 2));
+    }
+	
+    async function second() {
+        let readline = require('readline-sync');       
+        let no = readline.question('Write the id of the liga you want to see?\n');
+
+        let response = await fetch("http://localhost:9999/api/liga/"+no, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept-Content-Type": "application/json"
+            },
+        });
+        let status = await response.status;
+        if (status != 200) {
+            console.log("Error invalid response: HTML Response: "+status+"\nTry again");
+            second();
+        } else {
+            let json = await response.json();
+            console.log(JSON.stringify(json, null, 2));
+        }
     }
 
-    let response = await fetch("http://localhost:9999/api/liga/", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept-Content-Type": "application/json"
-        },
-    });
-
-    let json = await response.json();
-    console.log(JSON.stringify(json, null, 2));
-	
-	let readline = require('readline-sync');       
-    let no = readline.question('Write the id of the liga you want to see?\n');
-
-    response = await fetch("http://localhost:9999/api/liga/"+no, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept-Content-Type": "application/json"
-        },
-    });
-
-    json = await response.json();
-    console.log(JSON.stringify(json, null, 2));
+    await first();
+    await second();
 }
 a();
