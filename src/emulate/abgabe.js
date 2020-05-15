1c1cfb99a875d3a290dadf479d8991a7d63c3a9f
mulc <buermarc2@gmail.com>
2020-05-15 20:44:42 +0200

    abgabe.js

--- src/emulate/abgabe.js
+++ src/emulate/abgabe.js
@@ -1,5 +1,7 @@
 "use strict";
 const fetch = require("node-fetch");
+const readline = require('readline-sync');       
+
 async function a () {
 
     async function first() {
@@ -14,11 +16,18 @@ async function a () {
 
         let json = await response.json();
         console.log(JSON.stringify(json, null, 2));
+        return json;
     }
 	
-    async function second() {
-        let readline = require('readline-sync');       
+    async function second(json) {
         let no = readline.question('Write the id of the liga you want to see?\n');
+
+        let res = json._embedded.liga[no-1] 
+
+        if (res == null) {
+            console.log("Invalid Liga id. Please try again\n");
+            return second(json);
+        }
 
         let response = await fetch("http://localhost:9999/api/liga/"+no, {
             method: "GET",
@@ -27,17 +36,55 @@ async function a () {
                 "Accept-Content-Type": "application/json"
             },
         });
+
         let status = await response.status;
         if (status != 200) {
             console.log("Error invalid response: HTML Response: "+status+"\nTry again");
-            second();
+            await new Promise(r => setTimeout(r, 2000));
+            return second(json);
+        } else {
+            let json = await response.json();
+            console.log(JSON.stringify(json, null, 2));
+            return json;
+        }
+    }
+
+    async function third(json) {
+        //TODO solve hardcoded string
+        let stringHelp = ":: self \n:: liga \n:: divisions\n";
+
+        let readline = require('readline-sync');       
+        let no = readline.question('Write the name of the link you want to see?\n'+stringHelp);
+        
+        let url = await json._links[no];
+
+        if (url == null) {
+            console.log("Invalid link name. Please use one of the following links\n"+stringHelp)
+            return third(json);
+        }
+
+        let response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept-Content-Type": "application/json"
+            },
+        });
+
+        let status = await response.status;
+        if (status != 200) {
+            console.log("Error invalid response: HTML Response: "+status+"\nTry again");
+            await new Promise(r => setTimeout(r, 2000));
+            return third(json);
         } else {
             let json = await response.json();
             console.log(JSON.stringify(json, null, 2));
         }
+
     }
 
-    await first();
-    await second();
+    let json = await first();
+    json = await second(json);
+    await third(json);
 }
 a();
